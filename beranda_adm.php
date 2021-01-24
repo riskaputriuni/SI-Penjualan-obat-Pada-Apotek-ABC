720982b8ae7dc1895c617de72fee3fd0156cda97
riskaputriuni <77909661+riskaputriuni@users.noreply.github.com>
2021-01-23 20:33:10 -0800

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,39 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat datang kembali 
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator Sistem Informasi Penjualan Obat</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/apotek.png"></img></p>
+                          <p class="label-title" align="center"><strong>PENJUALAN OBAT</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Penjualan Obat.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data obat, pengelolaan laporan pembelian, <br>
+                            dan juga pengelolaan laporan arsip serta peminjamannya.<br><br>
+                            Akses menu Menu Data pada bagian atas sistem untuk pengelolaan data obat dan data laporan pembelian obat.<br>
+                            Untuk mengelola laporan, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
